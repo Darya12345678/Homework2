7ad2e0efae77f98b4ebda75c7916199b28d2df31
Darya <darya.rempel@bk.ru>
2022-12-21 23:24:21 +0600

    Добавила поиск по номеру или имени

--- view.py
+++ view.py
@@ -1,10 +1,12 @@
 import ex1 as c
 
 def dialog_user():
-    print('1-просмотр контактов, 2-добавление контакта')
+    print('1-просмотр контактов, 2-добавление контакта, 3-поиск контакта по имени/номеру телефона/части номера')
     a = int(input('Введите число: '))
     if a == 1:
         c.contacts()
+    if a == 3:
+        c.contacts_search()
     elif a == 2:
         c.import_cont() 
     else:
